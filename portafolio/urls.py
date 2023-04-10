829f4e15a0f90374b3099144032f4a8a2fced7b1
Diego Córdoba <107233021+diegocordoba96@users.noreply.github.com>
2023-04-10 10:28:08 -0500

    Update urls.py

--- portafolio/urls.py
+++ portafolio/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import re_path
+from django.views.static import serve
+
 
 
 
